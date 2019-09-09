58258f17cea35510c9afb7c16c1a657bf6a8f509
Matt Heerspink <mheerspink75@gmail.com>
2019-09-09 08:37:00 -0500

    added dialog content text to contact form

--- src/Navigation/ContactForm.js
+++ src/Navigation/ContactForm.js
@@ -4,6 +4,7 @@ import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
 export default function FormDialog() {
@@ -29,6 +30,9 @@ export default function FormDialog() {
       >
         <DialogTitle id="form-dialog-title">Contact Info</DialogTitle>
         <DialogContent>
+          <DialogContentText>
+            Please enter your contact information and leave a comment...
+          </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
